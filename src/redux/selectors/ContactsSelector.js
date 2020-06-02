5df9fab67bd79ac630535d8f03f5c79e32b966c1
r.beniukh <r.beniukh@kt.ua>
2020-06-02 07:33:37 +0300

    add reselect

--- src/redux/selectors/ContactsSelector.js
+++ src/redux/selectors/ContactsSelector.js
@@ -1,5 +1,9 @@
+import { createSelector } from 'reselect';
+
 export const GET_ALL_CONTACTS = state => state.contacts;
-export const GET_FILTERED_CONTACTS = state =>
-  state.contacts.filter(contact => contact.name.toLowerCase().includes(state.filter.toLowerCase()));
 export const GET_FILTER_VALUE = state => state.filter;
 export const GET_EDIT = state => state.edit;
+
+export const GET_FILTERED_CONTACTS = createSelector([GET_ALL_CONTACTS, GET_FILTER_VALUE], (contacts, filter) =>
+  contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())),
+);
